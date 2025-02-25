91e88ef7046a01c37484d321f734d943b6649a67
CheonUng <cjsdnd9712@gmail.com>
2025-02-25 16:20:36 +0900

    변수 선언

---
+++ Ch1/src/ch1/Print.java
@@ -0,0 +1,9 @@
+package ch1;
+
+    // 늘 하던 명명 규칙 지켜라
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("허헣");
+    }
+}
---
+++ Ch1/src/ch1/VarEx1.java
@@ -0,0 +1,19 @@
+package ch1;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        byte age = 15, maxSpeed = 100;
+        System.out.println("나이: " + age + ", 최대속도: " + maxSpeed);
+
+        int a = 10, b = 20;
+        System.out.println(a + b);
+
+        long la = 11111111111111111L;
+
+        char ch1 = '기';
+        int chInt = ch1;
+        System.out.println(chInt);
+
+
+    }
+}
