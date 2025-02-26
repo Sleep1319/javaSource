50ba684d3ec6b254e166fd829956dcd520041a57
CheonUng <cjsdnd9712@gmail.com>
2025-02-26 15:53:39 +0900

    연산자자

--- Ch2_operator/src/op/SignOperEx1.java
+++ Ch2_operator/src/op/SignOperEx1.java
@@ -5,13 +5,26 @@ public class SignOperEx1 {
     public static void main(String[] args) {
         int a = 10, b = 4;
 
+        System.out.println("사칙");
         System.out.println(a + b);
         System.out.println(a - b);
         System.out.println(a * b);
         System.out.println(a / b);
+        System.out.println(a / (float)b);
         System.out.println(a % b);
+        System.out.println("비교");
         System.out.println(a > b);
         System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println("논리");
+        System.out.println(a < b && a < b);
+        System.out.println(a > b && a < b);
+        System.out.println(a > b && a > b);
+        System.out.println(a < b || a < b);
+        System.out.println(a > b || a < b);
+        System.out.println(a > b || a > b);
+        System.out.println("전 후");
         System.out.println(+a);
         System.out.println(-b);
         System.out.println(a + "전위" + ++a);
@@ -20,5 +33,6 @@ public class SignOperEx1 {
         System.out.println(--b);
         System.out.println(b--);
         System.out.println(b);
+        System.out.println("스트링은 그리 배웠는데 모르면 어후");
     }
 }
