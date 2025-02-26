6a941ad4be47cb05336b42267a1a89693278001d
CheonUng <cjsdnd9712@gmail.com>
2025-02-26 17:23:12 +0900

    if

---
+++ Ch3_condition/src/ifTest.java
@@ -0,0 +1,20 @@
+public class ifTest {
+    public static void main(String[] args) {
+        
+        int a = 10, b = 4;
+
+        if(a > b) {
+            System.out.println("트루");
+        } 
+
+        if(a < b) {
+            System.out.println("거짓");
+        }
+        System.out.println("---------");
+
+        int c = (int) (Math.random() * 6) + 1;
+        System.out.println(c);
+        int d = (int) (Math.random() * 100) + 1;
+        System.out.println(d);
+    }
+}
